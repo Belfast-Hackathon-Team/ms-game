2d4082d18a83ff7af90cafcb0bff443ca678fbac
Ben Connor <benmarkconnor@gmail.com>
2021-02-06 21:45:41 +0000

    Minor changes

--- code.lua
+++ code.lua
@@ -88,5 +88,5 @@ function Draw()
   DrawIslands()
   ChangeBoatSprite()
   DrawBoatPos()
-  DrawText( string.format("Time: %.2f", (TimeLeft / 100)), 25, 50, DrawMode.UI, "large", 14 )
+  DrawText( string.format("Time: %.0f", (TimeLeft / 100)), 25, 50, DrawMode.UI, "large", 14 )
 end
