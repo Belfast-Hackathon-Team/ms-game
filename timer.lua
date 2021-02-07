2ae21f86b960636e923ff659c3f4e335db4c6e8a
Ben Connor <benmarkconnor@gmail.com>
2021-02-07 09:37:56 +0000

    Minor timer changes

--- timer.lua
+++ timer.lua
@@ -1,14 +1,11 @@
-timeLeft = 6000;
-started = false;
+local timeLeft = 6000;
+local started = false;
 
 function AddTime(time)
   timeLeft += time
 end
 
 function TickTimer(amt)
-  if(timeLeft - amt <= 0) then
-    return 0
-  end
   timeLeft = timeLeft - amt
   return timeLeft
 end
