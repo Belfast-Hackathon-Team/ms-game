11ca5a23f0d273be87e69b2659e58434366c0acc
jasonbqub <78610288+jasonbqub@users.noreply.github.com>
2021-02-07 03:56:39 +0000

    Removed conflict error + uncommented StartScreen()

--- code.lua
+++ code.lua
@@ -53,16 +53,12 @@ Player = {
 }
 
 ListOfActiveIslands = {}
-<<<<<<< HEAD
 local TimeLeft = 1
 GameStarted = false
 GameFinished = false
-=======
 local TimeLeft = 6000
 GameStarted = false
 GameFinished = false
-
->>>>>>> 1f98511c8b8b24ece73bc61dc852595e031e3702
 
 function Init()
   BackgroundColor( 2 )
@@ -73,7 +69,7 @@ end
 
 function Update(timeDelta)
   if not (GameStarted) and not (GameFinished) then
-    --StartScreen()
+    StartScreen()
     return
   elseif (GameFinished) then
     EndScreen()
